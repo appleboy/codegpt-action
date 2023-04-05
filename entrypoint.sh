3cf2523297fc79968559feb8db006a119cbca3e5
Bo-Yi.Wu <appleboy.tw@gmail.com>
2023-04-05 18:02:06 +0800

    update
    Signed-off-by: Bo-Yi.Wu <appleboy.tw@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -eu
 
 export PLATFORM="github"
 
+git config --global --add safe.directory /github/workspace
 codegpt version
 # ls -al
-codegpt review --help
-git config --global --add safe.directory /github/workspace
+# codegpt review --help
 # git log
 codegpt review --amend
